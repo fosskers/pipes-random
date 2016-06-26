dd4d1717dbf0f33ed138a883a9a2cd01a517e79c
Colin Woodbury <colingw@gmail.com>
2016-06-26 16:10:50 -0700

    Settled on API

--- Pipes/Random.hs
+++ Pipes/Random.hs
@@ -7,6 +7,33 @@
 -- Maintainer: Colin Woodbury <cwoodbury@azavea.com>
 --
 -- Producers for handling randomness.
+--
+-- == Random Numbers
+--
+-- == Random Elements from Containers
+-- We expose the functions `finite` and `endless` for randomly Producing
+-- elements from a collection.
+--
+-- `finite` will only Produce until each of its elements have been yielded once.
+-- Making a shuffle function then is easy:
+--
+-- > import           Data.Vector (Vector)
+-- > import qualified Pipes.Prelude as P
+-- >
+-- > shuffle :: Vector a -> IO [a]
+-- > shuffle = P.toListM . finite
+--
+-- `endless` on the other hand will endlessly Produce elements in any order.
+-- Repeats will likely appear long before each element has been yielded once.
+-- You can limit the number of results with `P.take`:
+--
+-- > import           Data.Vector (Vector)
+-- > import qualified Pipes.Prelude as P
+-- >
+-- > twenty :: Vector a -> Producer a IO ()
+-- > twenty v = endless v >-> P.take 20
+--
+-- For the time being, only `V.Vector`s are supported.
 
 module Pipes.Random
   ( -- * Pseudo-random Number Generators
@@ -21,7 +48,6 @@ module Pipes.Random
   ) where
 
 import           Control.Monad (forever)
-import           Control.Monad.Primitive (PrimState)
 import qualified Data.Vector.Generic as V
 import           Pipes
 import qualified Pipes.Prelude as P
@@ -30,13 +56,18 @@ import qualified System.Random.MWC.Distributions as R
 
 ---
 
--- | Produce a PRNG using system randomness.
-pool :: Producer (R.Gen (PrimState IO)) IO ()
-pool = lift (R.withSystemRandom @IO (pure . id)) >>= yield
+-- | A pseudo-random number generator, produced using system randomness.
+--
+-- `lift` this to transform it into a `Producer`.
+pool :: IO R.GenIO
+pool = R.createSystemRandom
 
 -- | Endlessly produce anything that's `R.Variate` from a uniform distribution.
+--
+-- * For integral types, the entire bit range is used.
+-- * For floating types, the range is (0,1], where 0 is specifically excluded.
 uniform :: R.Variate v => Producer v IO ()
-uniform = for pool $ \g -> forever (lift (R.uniform g) >>= yield)
+uniform = lift pool >>= \g -> forever (lift (R.uniform g) >>= yield)
 
 -- | Endlessly produce anything that's `R.Variate` from a uniform distribution,
 -- within some given range of values.
@@ -44,19 +75,19 @@ uniform = for pool $ \g -> forever (lift (R.uniform g) >>= yield)
 -- * For integral types, inclusive range is used.
 -- * For floating types, (a,b] is used.
 uniformR :: R.Variate v => (v, v) -> Producer v IO ()
-uniformR r = for pool $ \g -> forever (lift (R.uniformR r g) >>= yield)
+uniformR r = lift pool >>= \g -> forever (lift (R.uniformR r g) >>= yield)
 
 -- | Given a mean and a standard deviation, endlessly produce values from
 -- a normal (Gaussian) distribution.
 normal :: Double -> Double -> Producer Double IO ()
-normal m sd = for pool $ \g -> forever (lift (R.normal m sd g) >>= yield)
+normal m sd = lift pool >>= \g -> forever (lift (R.normal m sd g) >>= yield)
 
 -- | Given some `V.Vector`, produce its elements in a random order, once each.
 --
 -- > >>> P.toListM $ endless (V.fromList @Data.Vector.Vector ['a'..'z'])
 -- > "rkzpnwjfeqotvdlsaxiuhcbymg"
 finite :: V.Vector v a => v a -> Producer a IO ()
-finite = for pool . f
+finite l = lift pool >>= f l
   where f v g | V.null v = pure ()
               | otherwise = do
                   i <- lift $ R.uniformR (0, V.length v - 1) g
